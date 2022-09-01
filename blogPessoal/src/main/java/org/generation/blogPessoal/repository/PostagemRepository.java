9608d399e5f19912e02f65bf7837a14994dff9d2
unknown <Edusan17>
2022-09-01 12:32:39 -0300

    version 1.12.15

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,16 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+}
